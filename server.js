17b4f536a0656e0604e6d35d9621a658f5f60c57
Arti <arti.iosdev@gmail.com>
2021-04-29 22:38:11 -0700

    update port no

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -27,8 +29,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbworkout", {
 require("./routes/htmlroutes")(app);
 require("./routes/apiroutes")(app);
 
-http.listen((process.env.PORT || 5000), function(){
-  console.log('listening on *:5000');
+app.listen((process.env.PORT || 5000), function(){
+  console.log('listening on 5000');
 });
 
 /*app.listen(PORT, () => {
